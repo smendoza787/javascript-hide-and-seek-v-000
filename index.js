8d28068c375e282a6522ed02d1e8fef1602bf7b2
Sergio Mendoza <smendoza787@gmail.com>
2017-06-27 23:49:09 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div#nested div div div div.target');
+}
+
+function increaseRankBy(n) {
+  let rankedLis = document.querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < rankedLis.length; i++) {
+
+    rankedLis[i].innerHTML = (parseInt(rankedLis[i].innerHTML, 10) + n);
+  }
+}
+
+function deepestChild() {
+  let node = document.querySelector('#grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+}
